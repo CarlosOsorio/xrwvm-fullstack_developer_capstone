6baf80e393bc4f8ebafaf531bef24bd1ff030602
carlososorio <carlos.aosorio89@gmail.com>
2025-04-15 18:27:10 -0400

    lint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -33,7 +33,8 @@ ALLOWED_HOSTS = [
     # 'https://carlosaosori-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ]
 CSRF_TRUSTED_ORIGINS = [
-    # 'https://carlosaosori-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    # 'https://carlosaosori-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
