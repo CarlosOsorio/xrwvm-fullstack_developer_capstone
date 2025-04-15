249b486a64d9951fb5fcb50ddd5e19a64e6101fa
carlososorio <carlos.aosorio89@gmail.com>
2025-04-15 18:18:49 -0400

    lint

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,7 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost',
-    # 'https://carlosaosori-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
+    # 'https://carlosaosori-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    ]
 CSRF_TRUSTED_ORIGINS = [
     # 'https://carlosaosori-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai']
 
